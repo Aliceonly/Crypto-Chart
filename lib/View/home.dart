92bbe73ed5967367d6df735af723ddd941bd2205
Aliceonly <aliceonly@foxmail.com>
2023-12-31 23:18:38 +0800

    init: home scaffold

---
+++ lib/View/home.dart
@@ -0,0 +1,141 @@
+import "package:flutter/material.dart";
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    double myHeight = MediaQuery.of(context).size.height;
+    double myWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Container(
+        height: myHeight,
+        width: myWidth,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+              Color.fromARGB(255, 253, 225, 112),
+              Color(0xffFBC700)
+            ])),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: myHeight * 0.03),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: myWidth * 0.02, vertical: myHeight * 0.005),
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Text(
+                      'Main portfolio',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Top 10 coins',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    'Exprimental',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: myWidth * 0.07),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '\$ 7,466.20',
+                    style: TextStyle(fontSize: 35),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(myWidth * 0.02),
+                    height: myHeight * 0.05,
+                    width: myWidth * 0.1,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                    child: Image.asset(
+                      "assets/icons/5.1.png",
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: myWidth * 0.07),
+              child: Row(
+                children: [
+                  Text(
+                    '+162% all time',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: myHeight * 0.02,
+            ),
+            Container(
+              height: myHeight * 0.7,
+              width: myWidth,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50)),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: myHeight * 0.03,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: myWidth * 0.08),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Assets',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Icon(Icons.add)
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                      child: ListView.builder(itemBuilder: (context, index) {
+                    return Text('data');
+                  }))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
